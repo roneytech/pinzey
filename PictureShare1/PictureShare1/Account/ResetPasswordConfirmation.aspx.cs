526ec2b7b428e554ddec2e399c7f93dc0657c49c
roneytech <morgan.roney@roneytechnologies.com>
2015-10-10 13:32:44 -0700

    Revert "Checking in original version"
    This reverts commit aa008008ef8d5b9d7862c406a23c4c154de3e4c9.

--- PictureShare1/PictureShare1/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace PictureShare1.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- PictureShare1/PictureShare1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PictureShare1.Global" Language="C#" %>
